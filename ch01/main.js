c3d26ccf259ce2eb919b278df25e16a9d1ce5c6a
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-01-06 16:39:40 +0900

    nodeJS setup

---
+++ ch01/main.js
@@ -0,0 +1,17 @@
+// @ts-check
+// Formatting, Linting
+// Formatting : Prettier
+// Linting : ESLint
+
+/* eslint-disable-next-line no-console */
+console.log('Hello world')
+
+/* eslint-disable-next-line */
+// console.log(eval()) // 이상한 코드도 에러 체크 못하고 있음
+
+const x = 10
+console.log(x)
+
+// Type check
+const someString = 'Hello'
+console.log(someString)
